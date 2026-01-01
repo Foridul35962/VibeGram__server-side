70c8e2c619c1eab3960a9b3ecda69f38f6860438
Foridul35962 <foridulislam35962@gmail.com>
2026-01-01 11:23:46 +0600

    add token generator function

---
+++ src/utils/token.js
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+
+const generateToken = (userId) => {
+    return jwt.sign(
+        { userId },
+        process.env.TOKEN_SECRET,
+        { expiresIn: process.env.TOKEN_EXPIRY }
+    )
+}
+
+export default generateToken
