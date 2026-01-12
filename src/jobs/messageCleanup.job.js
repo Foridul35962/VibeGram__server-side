a79790fc8d29b7e9eab5812d4eaf6977e6a34443
Foridul35962 <foridulislam35962@gmail.com>
2026-01-13 02:49:20 +0600

    auto message clean functionality added

---
+++ src/jobs/messageCleanup.job.js
@@ -0,0 +1,38 @@
+import cron from 'node-cron'
+import Messages from '../models/Message.model.js'
+import cloudinary from '../config/cloudinary.js'
+
+const startMessageCleanupJob = ()=>{
+    let isRunning = false
+
+    cron.schedule('*/10 * * * *', async()=>{
+        if (isRunning) {
+            return
+        }
+        isRunning = true
+
+        try {
+            const expiryTime = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+
+            const expiredMessage = await Messages
+                .find({createdAt: {$lte: expiryTime}})
+                .sort({createdAt: 1})
+                .limit(30)
+            
+            for (const message of expiredMessage){
+                try {
+                    if (message.image.publicId) {
+                        await cloudinary.uploader.destroy(message.image.publicId)
+                    }
+                    await message.deleteOne()
+                } catch (error) {
+                    console.error('message cleanup failed', message._id, error)
+                }
+            }
+        } finally {
+            isRunning = false
+        }
+    })
+}
+
+export default startMessageCleanupJob
