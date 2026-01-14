4fd9c2081f302bdd1ce2789df4e0211d2e50cd26
Foridul35962 <foridulislam35962@gmail.com>
2026-01-15 01:53:07 +0600

    socket io initialize and online status update

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './src/config/db.js'
 import app from './src/app.js'
+import http from 'http'
+import { Server } from 'socket.io'
+import socketHandler from './src/config/socket.js'
 
 
 const PORT = process.env.PORT || 3000
 
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true
+    }
+})
+
+app.set('io', io)
+socketHandler(io)
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`server is running on http://localhost:${PORT}`)
     })
 }).catch(() => {
---
+++ src/config/socket.js
@@ -0,0 +1,76 @@
+import Messages from "../models/Message.model.js"
+
+const userSocket = new Map()
+
+const notifyChatPartners = async (io, userId, online) => {
+    const sentTo = await Messages.distinct('receiver', { sender: userId })
+    const receivedFrom = await Messages.distinct('sender', { receiver: userId })
+
+    const partners = [...new Set([...sentTo, ...receivedFrom])]
+
+    partners.forEach(partnerId => {
+        io.to(`user:${partnerId}`).emit('presence:update', {
+            userId,
+            online
+        })
+    })
+}
+
+const isUserOnline = (userId) => {
+    return userSocket.has(String(userId))
+}
+
+
+const socketHandler = (io) => {
+    io.on('connection', (socket) => {
+        socket.on('identity', async ({ userId }) => {
+            userId = String(userId)
+
+            if (socket.data.userId) return
+
+            socket.data.userId = userId
+
+            socket.join(`user:${userId}`)
+
+            if (!userSocket.has(userId)) {
+                userSocket.set(userId, new Set())
+            }
+
+            const set = userSocket.get(userId)
+
+            const wasOffline = set.size === 0
+            set.add(socket.id)
+
+            const sentTo = await Messages.distinct('receiver', { sender: userId })
+            const receivedFrom = await Messages.distinct('sender', { receiver: userId })
+            const partners = [...new Set([...sentTo, ...receivedFrom])]
+
+            const onlinePartners = partners.filter(pid => isUserOnline(pid))
+
+            socket.emit('presence:initial', {
+                onlineUsers: onlinePartners
+            })
+
+            if (wasOffline) {
+                notifyChatPartners(io, userId, true)
+            }
+        })
+
+        socket.on('disconnect', () => {
+            const userId = socket.data.userId
+            if (!userId) return
+
+            const set = userSocket.get(userId)
+            if (!set) return
+
+            set.delete(socket.id)
+
+            if (set.size === 0) {
+                userSocket.delete(userId)
+                notifyChatPartners(io, userId, false)
+            }
+        })
+    })
+}
+
+export default socketHandler
