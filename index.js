1ddfa51fd288f880f85eca811fa5a8da8a8c2c20
Foridul35962 <foridulislam35962@gmail.com>
2026-01-18 02:17:44 +0600

    debug database connect and jobs

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import app from './src/app.js'
 import http from 'http'
 import { Server } from 'socket.io'
 import socketHandler from './src/config/socket.js'
+import startStoryCleanupJob from './src/jobs/storyCleanup.job.js'
+import startMessageCleanupJob from './src/jobs/messageCleanup.job.js'
 
 
 const PORT = process.env.PORT || 3000
@@ -22,9 +24,13 @@ app.set('io', io)
 socketHandler(io)
 
 connectDB().then(() => {
+    //jobs
+    startStoryCleanupJob()
+    startMessageCleanupJob()
+
     server.listen(PORT, () => {
         console.log(`server is running on http://localhost:${PORT}`)
     })
-}).catch(() => {
-    console.log('server connection failed')
+}).catch((err) => {
+    console.log('server connection failed', err)
 })
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose'
 
+mongoose.set("bufferCommands", false);
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/vibegram`)
-            .then(() => {
-                console.log('database is connected')
-            })
+        await mongoose.connect(`${process.env.MONGODB_URL}/vibegram`, {
+            serverSelectionTimeoutMS: 30000,
+        })
+        console.log('database is connected')
     } catch (error) {
         console.log(`database connected failed`, error)
+        throw error;
     }
 }
 
