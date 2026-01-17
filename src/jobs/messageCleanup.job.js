4efc745f62b7dc4fb09f9e632aebc7e68edb2cd2
Foridul35962 <foridulislam35962@gmail.com>
2026-01-18 02:42:51 +0600

    message cleanup jobs debug

--- src/jobs/messageCleanup.job.js
+++ src/jobs/messageCleanup.job.js
@@ -1,11 +1,12 @@
 import cron from 'node-cron'
 import Messages from '../models/Message.model.js'
 import cloudinary from '../config/cloudinary.js'
+import Conversations from '../models/Conversations.model.js'
 
-const startMessageCleanupJob = ()=>{
+const startMessageCleanupJob = () => {
     let isRunning = false
 
-    cron.schedule('*/10 * * * *', async()=>{
+    cron.schedule('*/10 * * * *', async () => {
         if (isRunning) {
             return
         }
@@ -15,20 +16,30 @@ const startMessageCleanupJob = ()=>{
             const expiryTime = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
 
             const expiredMessage = await Messages
-                .find({createdAt: {$lte: expiryTime}})
-                .sort({createdAt: 1})
+                .find({ createdAt: { $lte: expiryTime } })
+                .sort({ createdAt: 1 })
                 .limit(30)
-            
-            for (const message of expiredMessage){
+
+            for (const message of expiredMessage) {
                 try {
                     if (message.image.publicId) {
                         await cloudinary.uploader.destroy(message.image.publicId)
                     }
+
+                    await Conversations.updateMany(
+                        { messages: message._id },
+                        { $pull: { messages: message._id } }
+                    )
+
                     await message.deleteOne()
                 } catch (error) {
                     console.error('message cleanup failed', message._id, error)
                 }
             }
+
+            await Conversations.deleteMany({
+                messages: { $size: 0 }
+            })
         } finally {
             isRunning = false
         }
